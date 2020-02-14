d6b2942b0e53132d7038a7b56f7231f23f05dcaa
Gabi Viana <gabigviana@gmail.com>
2020-02-14 22:36:50 +0100

    fixed script and style

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
-fetch("https://kea-alt-del.dk/t5/api/productlist")
+// 3. put alcohol
+// 4. add css animation - "magic"
+// 5. fix width
+// 6. change the order on the  + fix nav:hover
+// 7. make it go to footer one kontakt is pressed
 
-    .then(function (response) {
-        return response.json()
-    })
 
-    .then(function (data) {
-        showData(data)
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+});
+
+
+
+fetch("http://kea-alt-del.dk/t5/api/categories")
+    .then(res => res.json())
+    .then(createCategories)
+
+
+
+function createCategories(data) {
+    console.log(data)
+    data.forEach(function (oneCat) {
+
+        const a = document.createElement("a");
+        a.setAttribute("href", `#${oneCat}`);
+        a.textContent = oneCat;
+        document.querySelector("#wrapper>header>nav").appendChild(a);
+
+
+        const section = document.createElement("section");
+        section.id = oneCat;
+        section.classList.add("menu-section");
+        const h2 = document.createElement("h2");
+        h2.textContent = oneCat;
+
+        document.querySelector("main").appendChild(h2);
+        document.querySelector("main").appendChild(section);
+
     })
+    getProducts();
+
+}
+
+
+
+function getProducts() {
+    fetch("https://kea-alt-del.dk/t5/api/productlist")
+
+        .then(function (response) {
+            return response.json()
+        })
+
+        .then(function (data) {
+            showData(data)
+        })
+}
 
 function showData(jsonData) {
+    console.log()
     jsonData.forEach(showSingleDish)
 }
 
@@ -21,21 +70,73 @@ function showSingleDish(dish) {
 
     copy.querySelector("h3").textContent = dish.name;
 
+    copy.querySelector(".short-description").textContent = dish.shortdescription;
 
-    if (dish.discount) {
-        copy.querySelector(".price-full span").textContent = dish.price;
-        const newPrice = Math.round(dish.price - dish.price * dish.discount / 100);
 
-        copy.querySelector(".price-full span").textContent = newPrice;
-    } else {
-        copy.querySelector(".price-discount").remove()
-        copy.querySelector(".price-full span").textContent = dish.price
+    if (dish.image) {
+        const base = "https://kea-alt-del.dk/t5/site/imgs/";
+        const smallImg = base + "small/" + dish.image + "-sm.jpg";
+
+        copy.querySelector("#dishImg").src = smallImg;
     }
 
 
-    const whoIsYourDaddy = document.querySelector("#starters")
-    whoIsYourDaddy.appendChild(copy)
+    if (dish.discount) {
+        copy.querySelector(".price-before span").textContent = dish.price;
+        const newPrice = Math.round(dish.price - dish.price * dish.discount / 100);
+
+        copy.querySelector(".price-after span").textContent = newPrice;
+    } else {
+        copy.querySelector(".price-before").remove()
+        copy.querySelector(".price-after span").textContent = dish.price
+    }
+
+    copy.querySelector("button").addEventListener("click", () => {
+        console.log("click", dish)
+        fetch(`https://kea-alt-del.dk/t5/api/product?id=${dish.id}`)
+            .then(res => res.json())
+            .then(showDetails);
+    });
+
+    console.log(`#${dish.category}`)
+    document.querySelector(`#${dish.category}`).appendChild(copy);
+
+
 }
+
+
+
+function showDetails(data) {
+    console.log(data)
+    modal.querySelector(".modal-name").textContent = data.name;
+    modal.querySelector(".modal-description").textContent = data.longdescription;
+    modal.querySelector(".modal-price").textContent = data.price;
+
+    if (data.vegetarian) {
+        modal.querySelector(".modal-vegetarian").style.display = "block";
+    }
+
+    else {
+        modal.querySelector(".modal-vegetarian").style.display = "none";
+    }
+
+
+ if (data.allergens.some(function(allergen)  {
+                          return allergen == "laktose";
+                          }))
+ {
+     modal.querySelector(".modal-dairy").style.display ="block";
+
+ } else {
+     modal.querySelector(".modal-dairy").style.display = "none";
+ }
+
+
+    modal.classList.remove("hide");
+
+}
+
+
 
 
 //var modal = document.getElementById("myModal");
